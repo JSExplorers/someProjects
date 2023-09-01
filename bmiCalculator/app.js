36ee2a33d03848600ad362545094536e81cf1de5
JSLearner2023 <teachermike2020@gmail.com>
2023-09-01 00:13:38 -0500

    BMI CALCULATOR

--- bmiCalculator/app.js
+++ bmiCalculator/app.js
@@ -1,5 +1,5 @@
 document.getElementById('bmiForm').addEventListener('submit', function (e) {
-  e.preventDefault;
+  e.preventDefault();
 
   const gender = document.getElementById('gender').value;
   const age = parseInt(document.getElementById('age').value);
@@ -7,23 +7,29 @@ document.getElementById('bmiForm').addEventListener('submit', function (e) {
   const heightInch = parseInt(document.getElementById('height-inch').value);
   const weight = parseFloat(document.getElementById('weight').value);
 
+  if (gender && age && heightFeet && heightInch && weight) {
+    const heightInMeter = (heightFeet * 12 + heightInch) * 0.0254;
 
+    const bmi = (weight / (heightInMeter * heightInMeter)).toFixed(2);
+    const result = document.getElementById('result');
 
+    let category = '';
 
-    if(gender && age && heightFeet&& heightInch&&weight){
-        const  heightInMeter = ((heightFeet * 12) + heightInch)*0.0254
-
-        const bmi = weight / (heightInMeter*heightInMeter);
-        const result = document.getElementById('result')
-
+    if (bmi < 18.5) {
+      category = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 25) {
+      category = 'Healthy Weight';
+    } else if (bmi >= 25 && bmi < 30) {
+      category = 'Overweight';
+    } else {
+      category = 'Obesity';
     }
 
+    result.innerHTML = `you BMI code is ${bmi} and you are ${category}`;
 
-
-
-
-
-
-
-
+    document.getElementById('age').value = '';
+    document.getElementById('height-feet').value = '';
+    document.getElementById('height-inch').value = '';
+    document.getElementById('weight').value = '';
+  }
 });
